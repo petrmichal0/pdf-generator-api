b7c58130cc2c50748f0d14da116f8bf9bfbe5cbf
Petr Michal <petrmichal0@gmail.com>
2025-04-01 00:36:05 +0200

    fix pupper settings

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -35,6 +35,8 @@ exports.generateTransactions = async (req, res, next) => {
 
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
