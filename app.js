9e3c14470ec4bb3ef54e392514feb864a7ea6a45
Petr Michal <petrmichal0@gmail.com>
2025-04-01 00:25:44 +0200

    error

--- app.js
+++ app.js
@@ -29,6 +29,16 @@ app.all("*", (req, res, next) => {
   );
 });
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 app.use(handleErrors);
 
 module.exports = app;
